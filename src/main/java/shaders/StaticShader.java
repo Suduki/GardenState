478886f69eae0140377cd98301ad9cad4fb1f950
Olle <olle.lenti@gmail.com>
2018-10-23 15:51:45 +0200

    Make Maven project

--- src/main/java/shaders/StaticShader.java
+++ src/main/java/shaders/StaticShader.java
@@ -8,8 +8,8 @@ import toolbox.Maths;
 
 public class StaticShader extends ShaderProgram {
 	
-	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
-	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	private static final String VERTEX_FILE = "src/main/java/shaders/vertexShader.txt";
+	private static final String FRAGMENT_FILE = "src/main/java/shaders/fragmentShader.txt";
 
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;
--- src/main/java/shaders/TerrainShader.java
+++ src/main/java/shaders/TerrainShader.java
@@ -7,8 +7,8 @@ import entities.Light;
 import toolbox.Maths;
 
 public class TerrainShader extends ShaderProgram {
-	private static final String VERTEX_FILE = "src/shaders/terrainVertexShader.txt";
-	private static final String FRAGMENT_FILE = "src/shaders/terrainFragmentShader.txt";
+	private static final String VERTEX_FILE = "src/main/java/shaders/terrainVertexShader.txt";
+	private static final String FRAGMENT_FILE = "src/main/java/shaders/terrainFragmentShader.txt";
 
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;
